d99887e7d2693653788e6f37e1948a473721998c
arumanuevo <glipischeck@gmail.com>
2026-04-04 19:46:42 -0300

    fsdfsdf

--- resources/views/obras/show.blade.php
+++ resources/views/obras/show.blade.php
@@ -241,7 +241,7 @@
                                                         <span class="tool-badge">{{ $notasNoLeidas }}</span>
                                                     @endif
                                                 </div>
-                                                <div class="tool-text">Bandeja de Entrada</div>
+                                                <div class="tool-text">Bandeja de Entraddda</div>
                                             </a>
                                         </div>
                                         @endif
